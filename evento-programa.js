03f7d6da8112292a00f1ee53e48b41187ee72878
Guilherme Veloso <guilherme.veloso12147@gmail.com>
2022-07-04 21:55:28 -0300

    codigo atividade 2

---
+++ evento-programa.js
@@ -0,0 +1,23 @@
+alert ("Boas vindas a Savinis,faça sua reserva a seguir")
+var num = ("/")
+var nome = prompt("Insira seu Nome Completo")
+let participantes = ["João", "Sara", "Fernando", "Matheus", ]
+var idade = prompt("Insira sua idade")
+ if (idade >= 18) {var data = prompt("Insira o dia desejado")
+if (data >= 01) {var data1 = prompt("Insira o mês desejado")
+if (data1 > 06) {if (participantes.length < 100) { (alert("Sua reserva foi marcada para "+data+num+data1)) 
+alert(participantes)
+} else {alert("Numero de participantes excedido!")
+  
+}
+  
+} else {alert("Mês invalido")
+  
+}
+  
+} else {alert("Dia invalido")
+  
+}  
+ } else {alert("Reservas somente acima de 18")
+  
+ }
