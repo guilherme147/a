2e05010b4a718bb08521af7b81abc23d53d0c9c9
Guilherme Veloso <guilherme.veloso12147@gmail.com>
2022-07-05 21:58:05 -0300

    trabalho encontro remoto 1

---
+++ SENAI-LOGICA-PROGRAMACAO/resolucao-problemas/aa.js
@@ -0,0 +1,28 @@
+//var nome = "Guilherme" // Quando digitamos no próprio codigo, chamamos de hard code
+//let nomeCompleto = "Guilherme Veloso" //camelcase
+//todo software possui entrada, processamento e saída.
+//console.log(nome);
+
+//let nome = prompt("digite seu nome")
+//let valor1 = 5;
+//let valor2 = 3;
+//console.log(valor1+valor2)
+//var nota1 = 7
+//var nota2 = 9
+//var nota3 = 5
+//var notaMedia = (nota1+nota2+nota3/3)
+//if (notaMedia => 21) {console.log("aprovado")
+    
+//} else {console.log("reprovado")
+    
+//} 
+
+let nota1 = parseFloat(prompt("Digite sua primeira nota"))
+let nota2 = parseFloat(prompt("Digite sua segunda nota"))
+let nota3 = parseFloat(prompt("Digite sua terceira nota"))
+var notaMedia = (nota1+nota2+nota3)
+if (notaMedia => 21 ) {console.log("Você passou!")
+    
+} else {console.log("voce está reprovado!")
+    
+}
